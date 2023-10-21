3d93f31333f8a9a94bff754b4332b766e43133c7
Jaxson Madison <a02336244@aggies.usu.edu>
2023-10-20 19:29:38 -0600

    Setup the option to upload to dynamo as well as created a flattening function

--- consumer.py
+++ consumer.py
@@ -12,8 +12,20 @@ s3 = session.client('s3')
 BUCKET_REQUESTS = 'usu-cs5260-percy-requests'
 BUCKET_WEB = 'usu-cs5250-percy-web'
 
-def process_requests():
-    while True:
+
+def flatten_attributes(widget_request):
+    # Start with the existing widget_request (excluding 'otherAttributes')
+    flattened_data = {key: value for key, value in widget_request.items() if key != "otherAttributes"}
+
+    # Flatten 'otherAttributes'
+    for attribute in widget_request.get("otherAttributes", []):
+        flattened_data[attribute["name"]] = attribute["value"]
+
+    return flattened_data
+
+
+def process_requests(destination):
+   while True:  # This is the outer loop that will keep the function running indefinitely
         try:
             # List the objects within the specified bucket
             response = s3.list_objects_v2(Bucket=BUCKET_REQUESTS)
@@ -21,7 +33,8 @@ def process_requests():
             # Check if the bucket is empty
             if response['KeyCount'] == 0:
                 print("No requests to process. Waiting for new requests...")
-                time.sleep(1)
+                time.sleep(0.1)  # Wait for 100ms
+                continue  # This will skip the rest of the loop and start from the beginning
 
             # If there are objects, we'll only handle the first one
             first_object = response['Contents'][0]
@@ -34,34 +47,56 @@ def process_requests():
             #print(widget_request)
             print(f"Working on: {widget_request['widgetId']} Type: {widget_request['type']}")
             time.sleep(1)
+            if destination == "bucket":
+                # Perform the operation based on the request type
+                if widget_request['type'] == 'create':
+                    widget_id = str(uuid.uuid4())  # Create a new UUID for the widget
+    
+                    # Incorporate all data from the widget request into the widget_object
+                    widget_object = widget_request.copy()
+                    widget_object["widget_id"] = widget_id
+    
+                    # Compute the owner key
+                    owner_key = widget_request['owner'].replace(" ", "-").lower()
+    
+                    # Upload the new widget object to the web bucket
+                    s3.put_object(Body=json.dumps(widget_object), Bucket=BUCKET_WEB, Key=f'widgets/{owner_key}/{widget_id}')
+                    print(f"UPLOADED with key: widgets/{owner_key}/{widget_id}")
+                    time.sleep(1)
+    
+                elif widget_request['type'] == 'update':
+                    widget_id = widget_request['widgetId']
+                    new_data = widget_request['otherAttributes']
+    
+                elif widget_request['type'] == 'delete':
+                    widget_id = widget_request['widgetId']
+                    # Delete the specified widget object
+                    #s3.delete_object(Bucket=BUCKET_WEB, Key=f'widgets/{widget_id}')
+    
+                else:
+                    print(f"Unknown request type: {widget_request['type']}")
+            elif destination == "dynamo":
+                dynamodb = session.resource('dynamodb')
+                table = dynamodb.Table('widgets') 
 
-            # Perform the operation based on the request type
-            if widget_request['type'] == 'create':
-                widget_id = str(uuid.uuid4())  # Create a new UUID for the widget
-
-                # Incorporate all data from the widget request into the widget_object
-                widget_object = widget_request.copy()
-                widget_object["widget_id"] = widget_id
-
-                # Compute the owner key
-                owner_key = widget_request['owner'].replace(" ", "-").lower()
-
-                # Upload the new widget object to the web bucket
-                s3.put_object(Body=json.dumps(widget_object), Bucket=BUCKET_WEB, Key=f'widgets/{owner_key}/{widget_id}')
-                print(f"UPLOADED with key: widgets/{owner_key}/{widget_id}")
-                time.sleep(1)
-
-            elif widget_request['type'] == 'update':
-                widget_id = widget_request['widgetId']
-                new_data = widget_request['otherAttributes']
-
-            elif widget_request['type'] == 'delete':
-                widget_id = widget_request['widgetId']
-                # Delete the specified widget object
-                #s3.delete_object(Bucket=BUCKET_WEB, Key=f'widgets/{widget_id}')
-
-            else:
-                print(f"Unknown request type: {widget_request['type']}")
+                if widget_request['type'] == 'create':
+                    widget_id = str(uuid.uuid4())  # Create a new UUID for the widget
+                    widget_request["id"] = widget_id  # Change here
+                    flattened_widget = flatten_attributes(widget_request)
+                    table.put_item(Item=flattened_widget)  # Store the flattened widget in the DynamoDB table
+                    print(f"Widget {widget_id} added to DynamoDB.")
+    
+                elif widget_request['type'] == 'update':
+                    widget_id = widget_request['widgetId']
+                    new_data = widget_request['otherAttributes']
+    
+                elif widget_request['type'] == 'delete':
+                    widget_id = widget_request['widgetId']
+                    # Delete the specified widget object
+                    #s3.delete_object(Bucket=BUCKET_WEB, Key=f'widgets/{widget_id}')
+    
+                else:
+                    print(f"Unknown request type: {widget_request['type']}")
 
             # Delete the request object from the bucket
             s3.delete_object(Bucket=BUCKET_REQUESTS, Key=file_key)
@@ -79,4 +114,6 @@ def process_requests():
             break
 
 if __name__ == "__main__":
-    process_requests()
+    destination = "dynamo"
+    #destination = "bucket"
+    process_requests(destination)
